29f6acb6b8edfb5ad22ebadc11aafe25b69fd242
Brennan Pilcher <brennan.pilcher@gmail.com>
2018-02-03 19:51:32 -0500

    Fixed port to be usable with Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
 const app = require('./app.js');
+const port = process.env.port || 8080;
 
-http.createServer(app.handleRequest).listen(8000);
+http.createServer(app.handleRequest).listen(port);
